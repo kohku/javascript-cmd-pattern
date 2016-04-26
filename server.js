6ce5f8ad41fcc2f97decd041a16e5c16a38e8880
Cruz Vazquez, Koh Kulkan Daniel <koh.kulkan.daniel.cruzvazquez@one.verizon.com>
2016-04-26 15:40:58 -0500

    Node es6

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var app = express();
-var port = 8000;
+'use strict'
 
-app.use(express.static(__dirname + '/public'));
+let express = require('express')
+let app = express()
+let port = 8000
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.redirect('/index.html');
 })
 
 app.listen(port)
-console.log('App is running at port: ' + port);
+console.log(`App is running at port: ${port}`)
